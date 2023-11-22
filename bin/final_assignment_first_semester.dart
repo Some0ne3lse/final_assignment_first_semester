12712748c38aa4fb37f54bc0fa4119f09f8be297
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2023-11-22 20:49:11 +0000

    Add initial files

---
+++ bin/final_assignment_first_semester.dart
@@ -0,0 +1,5 @@
+import 'package:final_assignment_first_semester/final_assignment_first_semester.dart' as final_assignment_first_semester;
+
+void main(List<String> arguments) {
+  print('Hello world: ${final_assignment_first_semester.calculate()}!');
+}
---
+++ test/final_assignment_first_semester_test.dart
@@ -0,0 +1,8 @@
+import 'package:final_assignment_first_semester/final_assignment_first_semester.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
