ad54bbb982048b5ccda9e5c2591a435a5812ac7e
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2023-12-10 20:53:24 +0000

    Add small hint in a note

--- lib/text_files/interaction_text/notes.dart
+++ lib/text_files/interaction_text/notes.dart
@@ -11,7 +11,7 @@ String basementNote() {
 String letter() {
   return ('''
   The damn dog keeps chewing on my shoes.
-  I've kept it in the garden lately, since it wont leave my shoes alone.
+  I've kept it in the garden lately, since it wont leave my shoes or doughnuts alone.
   It also took my homework with all my important info. I don't know how to get it back!
   I'm starting to regret getting it, but I needed a watchdog.
   ''');
