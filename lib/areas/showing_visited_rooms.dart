b0fce72402effccee1d3835e6527fddcb36362b3
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-01-08 00:02:56 +0000

    Add inventory options to bedroom

--- lib/areas/showing_visited_rooms.dart
+++ lib/areas/showing_visited_rooms.dart
@@ -22,12 +22,14 @@ void visitingBedroomBobAlive() {
   print('1: Look at Bob');
   print('2: Examine the room');
   print('3: Go to the Living Room');
+  print('4: Look at inventory');
 }
 
 void visitingBedroomBobDead() {
   print('1: Look at Bobs body');
   print('2: Examine the room');
   print('3: Go to the Living Room');
+  print('4: Look at inventory');
 }
 
 void noRoomsVisitedEntrance() {
